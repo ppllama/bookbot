891a84407a95490c8a46efad4150587a738c5492
ppllama <neelapanchakan@gmail.com>
2025-02-12 15:20:01 +0530

    Added the final functionality. Program is complete

--- main.py
+++ main.py
@@ -1,11 +1,31 @@
 import wordcounter
 
+# def main():
+#     bookpath = "books/frankenstein.txt"
+#     print("--- Begin report of books/frankenstein.txt---\n")
+#     file_contents = wordcounter.get_book_path(bookpath)
+#     num_of_word = wordcounter.words(file_contents)
+#     print(f"{num_of_word} words found in the document\n")
+#     report = wordcounter.reports(file_contents)
+#     print(report)
+#     print("--- End report ---")
+    
+
+# if __name__ == "__main__":
+#     main()
+
 def main():
-    with open("books/frankenstein.txt") as f:
-        file_contents = f.read()
-        # print(file_contents)
-        num_of_word = wordcounter.words(file_contents)
-        print(num_of_word)
+    bookpath = "books/frankenstein.txt"
+    file_contents = wordcounter.get_book_path(bookpath)
+    num_of_word = wordcounter.words(file_contents)
+    report = wordcounter.reports(file_contents)
+    
+    print("--- Begin report of books/frankenstein.txt ---")
+    print()  # This adds one blank line
+    print(f"{num_of_word} words found in the document")
+    print()  # This adds one blank line
+    print(report)
+    print("--- End report ---")
 
 if __name__ == "__main__":
     main()
--- wordcounter.py
+++ wordcounter.py
@@ -2,3 +2,33 @@ def words(f):
     word = f.split()
     number_of_words = len(word)
     return(number_of_words)
+
+def repetitions(f):
+    characters = {}
+    for character in f:
+        lowered_character = character.lower()
+        if lowered_character not in characters:
+            characters[lowered_character] = 1
+        else:
+            characters[lowered_character] += 1
+    return characters
+
+def get_book_path(path):
+    with open(path) as f:
+        return f.read()
+
+def reports(file_contents):
+    char_counter = repetitions(file_contents)
+    list_of_dict = []
+    for key in char_counter:
+        if key.isalpha():
+            list_of_dict.append({"name":key, "num":char_counter[key]})
+    
+    def sort_on(dict):
+        return dict["num"]
+
+    list_of_dict.sort(reverse=True, key=sort_on)    
+    result = ""
+    for key in list_of_dict:
+        result += f"The '{key['name']}' character was found {key['num']} times\n"
+    return result
